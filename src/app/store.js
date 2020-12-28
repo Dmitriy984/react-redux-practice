43668c1bcc466af2205eb332b7ddcbdb3c693bd3
Dmitriy Mironov <dmitrymironov84@gmail.com>
2020-12-29 00:52:12 +0300

    Add save state to address bar

--- src/app/store.js
+++ src/app/store.js
@@ -1,13 +1,12 @@
 import { createStore } from 'redux';
 import counterReducer from '../features/counter/counterSlice';
-// import { Hash } from '../features/counter/counterSlice';
+import { Hash } from '../features/counter/counterSlice';
 
 let store = createStore(counterReducer)
 
 store.subscribe(() => {
-    // Hash.clear();
-    // Hash.add('value', store.getState().value);
-    document.cookie = `state=${store.getState().value}`;
+    Hash.clear();
+    Hash.add('value', store.getState().value);
 })
 
 export default store;
--- src/features/counter/counterSlice.js
+++ src/features/counter/counterSlice.js
@@ -1,62 +1,61 @@
-// export const Hash = {
-// 	// Получаем данные из адреса
-// 	get: function() {
-// 		let vars = {}, hash, splitter, hashes;
+export const Hash = {
+	// Get data from address
+	get: function() {
+		let vars = {}, hash, splitter, hashes;
 
-//         hashes = decodeURIComponent(window.location.hash.substr(1));
-//         splitter = '/';
+        hashes = decodeURIComponent(window.location.hash.substr(1));
+        splitter = '/';
 
 
-// 		if (hashes.length === 0) {return vars;}
-// 		else {hashes = hashes.split(splitter);}
+		if (hashes.length === 0) {return vars;}
+		else {hashes = hashes.split(splitter);}
 
-// 		for (var i in hashes) {
-// 			if (hashes.hasOwnProperty(i)) {
-// 				hash = hashes[i].split('=');
-// 				if (typeof hash[1] == 'undefined') {
-// 					vars['anchor'] = hash[0];
-// 				}
-// 				else {
-// 					vars[hash[0]] = hash[1];
-// 				}
-// 			}
-// 		}
-// 		return vars;
-// 	},
-// 	// Заменяем данные в адресе на полученный массив
-// 	set: function(vars) {
-// 		let hash = '';
-// 		for (var i in vars) {
-// 			if (vars.hasOwnProperty(i)) {
-// 				hash += '&' + i + '=' + vars[i];
-// 			}
-// 		}
+		for (var i in hashes) {
+			if (hashes.hasOwnProperty(i)) {
+				hash = hashes[i].split('=');
+				if (typeof hash[1] == 'undefined') {
+					vars['anchor'] = hash[0];
+				}
+				else {
+					vars[hash[0]] = hash[1];
+				}
+			}
+		}
+		return vars;
+	},
+	// Replace the data in the address with the received array
+	set: function(vars) {
+		let hash = '';
+		for (var i in vars) {
+			if (vars.hasOwnProperty(i)) {
+				hash += '&' + i + '=' + vars[i];
+			}
+		}
 
-// 		window.location.hash = hash.substr(1);
+		window.location.hash = hash.substr(1);
 
-// 	},
-// 	// Добавляем одно значение в адрес
-// 	add: function(key, val) {
-// 		const hash = this.get();
-// 		hash[key] = val;
-// 		this.set(hash);
-// 	},
-// 	// Удаляем одно значение из адреса
-// 	remove: function(key) {
-// 		const hash = this.get();
-// 		delete hash[key];
-// 		this.set(hash);
-// 	},
-// 	// Очищаем все значения в адресе
-// 	clear: function() {
-// 		this.set({});
-// 	},
-// };
+	},
+	// Add one value to the address
+	add: function(key, val) {
+		const hash = this.get();
+		hash[key] = val;
+		this.set(hash);
+	},
+	// Remove one value from the address
+	remove: function(key) {
+		const hash = this.get();
+		delete hash[key];
+		this.set(hash);
+	},
+	// Clearing all values in the address
+	clear: function() {
+		this.set({});
+	},
+};
 
-//const hashState = +Hash.get().value || 0;
-const cookiesState = +document.cookie.replace(/(?:(?:^|.*;\s*)state\s*\=\s*([^;]*).*$)|^.*$/, "$1") || 0;
+const hashState = +Hash.get().value || 0;
 
-function counterReducer(state = { value: cookiesState }, action) {
+function counterReducer(state = { value: hashState }, action) {
   switch (action.type) {
     case 'counter/incremented':
       return { value: state.value + 1 }
