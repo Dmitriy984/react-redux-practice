464bf89bcfcf0f4cb3ee7fd8882c79e578437118
Dmitriy Mironov <dmitrymironov84@gmail.com>
2020-12-29 00:57:39 +0300

    Delete side solution data

--- src/app/store.js
+++ src/app/store.js
@@ -1,12 +1,9 @@
 import { createStore } from 'redux';
 import counterReducer from '../features/counter/counterSlice';
-// import { Hash } from '../features/counter/counterSlice';
 
 let store = createStore(counterReducer)
 
 store.subscribe(() => {
-    // Hash.clear();
-    // Hash.add('value', store.getState().value);
     document.cookie = `state=${store.getState().value}`;
 })
 
